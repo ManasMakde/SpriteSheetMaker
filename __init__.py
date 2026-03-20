1884d6031f09b46d72a1759af84001433ca5e10c
ManasMakde <manasmakde@gmail.com>
2026-03-20 22:22:24 +0530

    Bumping to version 5.1.3

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
 bl_info = {
     "name": "Sprite Sheet Maker",
     "author": "Manas R. Makde",
-    "version": (5, 1, 2),
+    "version": (5, 1, 3),
     "description": "3D to 2D sprite sheet converter with optional pixelation"
 }
 
